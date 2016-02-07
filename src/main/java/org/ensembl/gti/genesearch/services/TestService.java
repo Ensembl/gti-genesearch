04955fc02af2ddfc2a10dc03f041f7f4ca794851
Dan Staines <dstaines@ebi.ac.uk>
2016-02-07 16:52:50 +0000

    Added a param example

--- src/main/java/org/ensembl/gti/genesearch/services/TestService.java
+++ src/main/java/org/ensembl/gti/genesearch/services/TestService.java
@@ -7,6 +7,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
  
 @Path("/test")
@@ -24,6 +25,17 @@ public class TestService {
   public Map<String,Object> sayHelloJson(@PathParam("name") String name) {
 	  Map<String,Object> output = new HashMap<>();
 	  output.put("name", name);
+      return output;
+  }
+
+  @GET
+  @Path("/echo")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Map<String,Object> echoJson(@QueryParam("name") String name, @QueryParam("age") int age, @QueryParam("address") String address) {
+	  Map<String,Object> output = new HashMap<>();
+	  output.put("name", name);
+	  output.put("age", age);
+	  output.put("address", address);
       return output;
   }
 
