88f1c0a997e4a50e8ba2baf612bfbeaa8ee15f69
Dan Staines <dstaines@ebi.ac.uk>
2016-02-22 19:31:37 +0000

    added blacklist support (by species name)

--- src/main/perl/lib/Bio/EnsEMBL/GTI/GeneSearch/Pipeline/JsonFileFactory.pm
+++ src/main/perl/lib/Bio/EnsEMBL/GTI/GeneSearch/Pipeline/JsonFileFactory.pm
@@ -4,12 +4,25 @@ use strict;
 use base ('Bio::EnsEMBL::Hive::Process');
 
 use File::Find;
+use File::Slurp;
+use Data::Dumper;
 
 sub run {
 	my $self     = shift @_;
 	my $base_dir = $self->param_required("dumps_dir");
 	my $files    = [];
-	find( sub { push @$files, { file => $File::Find::name } if m/\.json$/ }, $base_dir );
+	my $blacklist = {};
+	if(defined $self->param('blacklist')) {
+	    $blacklist = { map {chomp; $_=>1} read_file($self->param('blacklist'))};
+	}
+	find( 
+	    sub { 
+		if(m/\.json$/) {
+		    (my $name = $_) =~ s/([A-Za-z0-9_]+)\.json$/$1/; 
+		    push @$files, { file => $File::Find::name } unless $blacklist->{$name};
+		}
+	    }, 
+	    $base_dir );
 	$self->param( 'files', $files );
 	return;
 }
