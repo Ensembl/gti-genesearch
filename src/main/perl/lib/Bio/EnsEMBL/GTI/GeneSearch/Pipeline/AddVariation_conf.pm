6525f373b9cee19e6fb0449586fdc7fb34c19dc5
Dan Staines <dstaines@ebi.ac.uk>
2016-03-09 14:34:42 +0000

    added himem

--- src/main/perl/lib/Bio/EnsEMBL/GTI/GeneSearch/Pipeline/AddVariation_conf.pm
+++ src/main/perl/lib/Bio/EnsEMBL/GTI/GeneSearch/Pipeline/AddVariation_conf.pm
@@ -15,30 +15,35 @@ sub resource_classes {
 
 sub beekeeper_extra_cmdline_options {
   my ($self) = @_;
-  my $opts = '-reg_conf '.$self->o('registry');
+  my $opts = '-reg_conf ' . $self->o('registry');
   return $opts;
 }
 
 sub default_options {
   my ($self) = @_;
   return { %{ $self->SUPER::default_options() },
-           es_url    => 'http://gti-es-0.ebi.ac.uk:9200/' };
+           es_url => 'http://gti-es-0.ebi.ac.uk:9200/' };
 }
 
 sub pipeline_analyses {
   my ($self) = @_;
   return [ {
       -logic_name => 'VariationFactory',
-      -module     => 'Bio::EnsEMBL::GTI::GeneSearch::Pipeline::VariationFactory',
+      -module => 'Bio::EnsEMBL::GTI::GeneSearch::Pipeline::VariationFactory',
       -meadow_type => 'LOCAL',
       -input_ids   => [ {} ],
-      -parameters => { es_url => $self->o('es_url') },
-      -flow_into  => { '1'       => ['UpdateVariation'] } }, {
-      -logic_name  => 'UpdateVariation',
-      -module      => 'Bio::EnsEMBL::GTI::GeneSearch::Pipeline::UpdateVariation',
-      #'-meadow_type => 'LSF',
+      -parameters  => { es_url => $self->o('es_url') },
+      -flow_into   => { '1' => ['UpdateVariation'] } }, {
+      -logic_name => 'UpdateVariation',
+      -module     => 'Bio::EnsEMBL::GTI::GeneSearch::Pipeline::UpdateVariation',
       -hive_capacity => 25,
       -rc_name       => 'default',
+      -flow_into     => { -1 => 'UpdateVariationHiMem', },
+      -parameters    => { 'es_url' => $self->o('es_url') } }, {
+      -logic_name => 'UpdateVariationHiMem',
+      -module     => 'Bio::EnsEMBL::GTI::GeneSearch::Pipeline::UpdateVariation',
+      -hive_capacity => 25,
+      -rc_name       => 'himem',
       -parameters    => { 'es_url' => $self->o('es_url') } } ];
 }
 
