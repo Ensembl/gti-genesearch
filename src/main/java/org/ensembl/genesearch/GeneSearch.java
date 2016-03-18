a851971e82241ae21009dc85f3884b3d9c34029e
Dan Staines <dstaines@ebi.ac.uk>
2016-03-18 13:22:30 +0000

    added query sort

--- src/main/java/org/ensembl/genesearch/GeneSearch.java
+++ src/main/java/org/ensembl/genesearch/GeneSearch.java
@@ -1,83 +1,66 @@
 package org.ensembl.genesearch;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 
-import org.apache.commons.lang3.StringUtils;
-
 public interface GeneSearch {
 
-	public class GeneQuery {
+	public class QuerySort {
+		public static enum SortDirection {
+			ASC, DESC
+		};
 
-		public enum GeneQueryType {
-			TERM, RANGE, NESTED;
+		public final String field;
+		public final SortDirection direction;
+
+		public QuerySort(String field, SortDirection direction) {
+			this.field = field;
+			this.direction = direction;
 		}
-
-		private final String fieldName;
-		private final String[] values;
-		private final GeneQuery[] subQueries;
-		private final GeneQueryType type;
-
-		public GeneQuery(GeneQueryType type, String fieldName, String... values) {
-			this.type = type;
-			this.fieldName = fieldName;
-			this.values = values;
-			this.subQueries = null;
-		}
-
-		public GeneQuery(GeneQueryType type, String fieldName,
-				Collection<String> valuesC) {
-			this.type = type;
-			this.fieldName = fieldName;
-			this.values = valuesC.toArray(new String[valuesC.size()]);
-			this.subQueries = null;
-		}
-
-		public GeneQuery(GeneQueryType type, String fieldName,
-				GeneQuery... subQueries) {
-			this.type = type;
-			this.fieldName = fieldName;
-			this.values = null;
-			this.subQueries = subQueries;
-		}
-
-		public String getFieldName() {
-			return fieldName;
-		}
-
-		public String[] getValues() {
-			return values;
-		}
-
-		public GeneQueryType getType() {
-			return type;
-		}
-
-		public GeneQuery[] getSubQueries() {
-			return subQueries;
-		}
-
-		public String toString() {
-			if (type == GeneQueryType.NESTED) {
-				return StringUtils.join(
-						Arrays.asList(this.type, this.fieldName,
-								Arrays.asList(this.subQueries)), ":");
-			} else {
-				return StringUtils.join(
-						Arrays.asList(this.type, this.fieldName,
-								Arrays.asList(this.values)), ":");
-			}
-		}
-
 	}
 
+	/**
+	 * Retrieve all results matching the supplied queries
+	 * 
+	 * @param queries
+	 * @param fieldNames
+	 *            (if empty the whole document will be returned)
+	 * @return
+	 */
 	public List<Map<String, Object>> query(Collection<GeneQuery> queries,
 			String... fieldNames);
 
+	/**
+	 * Retrieve all results matching the supplied queries and process with the
+	 * supplied consumer
+	 * 
+	 * @param consumer
+	 * @param queries
+	 * @param fieldNames
+	 *            (if empty the whole document will be returned)
+	 * @return
+	 */
 	public void query(Consumer<Map<String, Object>> consumer,
 			Collection<GeneQuery> queries, String... fieldNames);
+
+	/**
+	 * Search with the supplied queries and return a summary object containing
+	 * results and facets
+	 * 
+	 * @param queries
+	 *            list of queries to combine with AND
+	 * @param output
+	 *            source fields to include
+	 * @param facets
+	 *            fields to facet over
+	 * @param limit
+	 *            number of hits to return
+	 * @return
+	 */
+	public QueryResult query(Collection<GeneQuery> queries,
+			List<String> output, List<String> facets, int limit,
+			List<QuerySort> sorts);
 
 }
