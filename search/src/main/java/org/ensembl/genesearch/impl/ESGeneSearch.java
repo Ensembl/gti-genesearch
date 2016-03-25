0eea081233f003c2e166f751e7bee11dbb9a668e
Dan Staines <dstaines@ebi.ac.uk>
2016-03-25 21:18:40 +0000

    restructured search to allow sorting on fetch and to suppress source by default

--- search/src/main/java/org/ensembl/genesearch/impl/ESGeneSearch.java
+++ search/src/main/java/org/ensembl/genesearch/impl/ESGeneSearch.java
@@ -60,35 +60,31 @@ public class ESGeneSearch implements GeneSearch {
 	}
 
 	@Override
-	public List<Map<String, Object>> fetch(Collection<GeneQuery> queries,
-			List<String> fieldNames, List<String> sorts) {
+	public List<Map<String, Object>> fetch(Collection<GeneQuery> queries, List<String> fieldNames, List<String> sorts) {
 		final List<Map<String, Object>> results = new ArrayList<>();
 		fetch(row -> results.add(row), queries, fieldNames, sorts);
 		return results;
 	}
 
 	@Override
-	public void fetch(Consumer<Map<String, Object>> consumer,
-			Collection<GeneQuery> queries, List<String> fieldNames,
+	public void fetch(Consumer<Map<String, Object>> consumer, Collection<GeneQuery> queries, List<String> fieldNames,
 			List<String> sorts) {
 
 		log.info("Building query");
-		QueryBuilder query = ESGeneSearchBuilder.buildQuery(queries
-				.toArray(new GeneQuery[queries.size()]));
+		QueryBuilder query = ESGeneSearchBuilder.buildQuery(queries.toArray(new GeneQuery[queries.size()]));
 
 		log.info("Starting query");
 		log.info(query.toString());
 
-		SearchRequestBuilder request = client.prepareSearch(index)
-				.setQuery(query).setScroll(new TimeValue(TIMEOUT))
+		SearchRequestBuilder request = client.prepareSearch(index).setQuery(query).setScroll(new TimeValue(TIMEOUT))
 				.setSize(SCROLL_SIZE);
 
-		request.setFetchSource(
-				fieldNames.toArray(new String[fieldNames.size()]), null);
+		addSorts(sorts, request);
+
+		request.setFetchSource(fieldNames.toArray(new String[fieldNames.size()]), null);
 
 		SearchResponse response = request.execute().actionGet();
-		log.info("Retrieved " + response.getHits().totalHits() + " in "
-				+ response.getTookInMillis() + " ms");
+		log.info("Retrieved " + response.getHits().totalHits() + " in " + response.getTookInMillis() + " ms");
 
 		processAllHits(consumer, response);
 		log.info("Retrieved all hits");
@@ -102,13 +98,12 @@ public class ESGeneSearch implements GeneSearch {
 	 * @param response
 	 * @return
 	 */
-	protected SearchResponse processAllHits(
-			Consumer<Map<String, Object>> consumer, SearchResponse response) {
+	protected SearchResponse processAllHits(Consumer<Map<String, Object>> consumer, SearchResponse response) {
 		// scroll until no hits are returned
 		while (true) {
 			processHits(consumer, response);
-			response = client.prepareSearchScroll(response.getScrollId())
-					.setScroll(new TimeValue(60000)).execute().actionGet();
+			response = client.prepareSearchScroll(response.getScrollId()).setScroll(new TimeValue(60000)).execute()
+					.actionGet();
 			if (response.getHits().getHits().length == 0) {
 				break;
 			}
@@ -122,76 +117,82 @@ public class ESGeneSearch implements GeneSearch {
 	 * @param consumer
 	 * @param response
 	 */
-	protected void processHits(Consumer<Map<String, Object>> consumer,
-			SearchResponse response) {
+	protected void processHits(Consumer<Map<String, Object>> consumer, SearchResponse response) {
 		for (SearchHit hit : response.getHits().getHits()) {
 			consumer.accept(processHit(hit));
 		}
 	}
 
 	@Override
-	public QueryResult query(Collection<GeneQuery> queries,
-			List<String> output, List<String> facets, int limit,
+	public QueryResult query(Collection<GeneQuery> queries, List<String> output, List<String> facets, int limit,
 			List<String> sorts) {
 		log.debug("Building query");
-		QueryBuilder query = ESGeneSearchBuilder.buildQuery(queries
-				.toArray(new GeneQuery[queries.size()]));
+		QueryBuilder query = ESGeneSearchBuilder.buildQuery(queries.toArray(new GeneQuery[queries.size()]));
 
 		log.info(query.toString());
 
-		SearchRequestBuilder request = client
-				.prepareSearch(index)
-				.setQuery(query)
-				.setFetchSource(output.toArray(new String[output.size()]), null)
-				.setSize(limit);
+		SearchRequestBuilder request = client.prepareSearch(index).setQuery(query)
+				.setFetchSource(output.toArray(new String[output.size()]), null).setSize(limit);
 
-		for (String sortStr : sorts) {
-			Sort sort = new Sort(sortStr);
-			log.info("Adding " + sort.direction + " sort on '" + sort.name
-					+ "'");
-			request.addSort(SortBuilders.fieldSort(sort.name)
-					.order(sort.direction).missing("_last"));
-		}
+		setFields(output, request);
 
-		for (String facet : facets) {
-			log.info("Adding facet on " + facet);
-			AbstractAggregationBuilder builder = ESGeneSearchBuilder
-					.buildAggregation(facet);
-			if (builder != null)
-				request.addAggregation(builder);
-		}
+		addSorts(sorts, request);
+
+		addFacets(facets, request);
+
 		log.info("Starting query (limit " + limit + ")");
 		log.debug("Query " + request.toString());
 
 		SearchResponse response = request.execute().actionGet();
-		log.info("Retrieved " + response.getHits().getHits().length + "/"
-				+ +response.getHits().totalHits() + " in "
+		log.info("Retrieved " + response.getHits().getHits().length + "/" + +response.getHits().totalHits() + " in "
 				+ response.getTookInMillis() + " ms");
 
-		return new QueryResult(response.getHits().getTotalHits(),
-				processResults(response), processAggregations(response));
+		return new QueryResult(response.getHits().getTotalHits(), processResults(response),
+				processAggregations(response));
 
 	}
 
+	private void setFields(List<String> output, SearchRequestBuilder request) {
+		if (output.isEmpty()) {
+			request.setFetchSource(false);
+		} else {
+			request.setFetchSource(output.toArray(new String[output.size()]), new String[0]);
+		}
+	}
+
+	private void addFacets(List<String> facets, SearchRequestBuilder request) {
+		for (String facet : facets) {
+			log.info("Adding facet on " + facet);
+			AbstractAggregationBuilder builder = ESGeneSearchBuilder.buildAggregation(facet);
+			if (builder != null)
+				request.addAggregation(builder);
+		}
+	}
+
+	private void addSorts(List<String> sorts, SearchRequestBuilder request) {
+		for (String sortStr : sorts) {
+			Sort sort = new Sort(sortStr);
+			log.info("Adding " + sort.direction + " sort on '" + sort.name + "'");
+			request.addSort(SortBuilders.fieldSort(sort.name).order(sort.direction).missing("_last"));
+		}
+	}
+
 	protected List<Map<String, Object>> processResults(SearchResponse response) {
-		return Arrays.stream(response.getHits().getHits())
-				.map(hit -> processHit(hit)).collect(Collectors.toList());
+		return Arrays.stream(response.getHits().getHits()).map(hit -> processHit(hit)).collect(Collectors.toList());
 	}
 
 	protected Map<String, Object> processHit(SearchHit hit) {
 		Map<String, Object> map = new HashMap<>();
 		map.put("id", hit.getId());
-		map.putAll(hit.getSource());
-		hit.getFields();
+		if (hit.getSource() != null)
+			map.putAll(hit.getSource());
 		return map;
 	}
 
-	protected Map<String, Map<String, Long>> processAggregations(
-			SearchResponse response) {
+	protected Map<String, Map<String, Long>> processAggregations(SearchResponse response) {
 		Map<String, Map<String, Long>> facetResults = new HashMap<>();
 		if (response.getAggregations() != null) {
-			for (Entry<String, Aggregation> facet : response.getAggregations()
-					.getAsMap().entrySet()) {
+			for (Entry<String, Aggregation> facet : response.getAggregations().getAsMap().entrySet()) {
 				log.debug("Getting facet on " + facet.getKey());
 				Map<String, Long> facetResult = new HashMap<>();
 				processAggregation(facetResult, facet.getValue());
@@ -201,8 +202,7 @@ public class ESGeneSearch implements GeneSearch {
 		return facetResults;
 	}
 
-	protected void processAggregation(Map<String, Long> facetResults,
-			Aggregation aggregation) {
+	protected void processAggregation(Map<String, Long> facetResults, Aggregation aggregation) {
 		if (Terms.class.isAssignableFrom(aggregation.getClass())) {
 			log.info("Processing terms aggregation " + aggregation.getName());
 			for (Bucket bucket : ((Terms) aggregation).getBuckets()) {
@@ -236,8 +236,7 @@ public class ESGeneSearch implements GeneSearch {
 			Matcher m = sortPattern.matcher(str);
 			if (m.matches()) {
 				name = m.group(2);
-				direction = "+".equals(m.group(1)) ? SortOrder.ASC
-						: SortOrder.DESC;
+				direction = "+".equals(m.group(1)) ? SortOrder.ASC : SortOrder.DESC;
 			} else {
 				name = str;
 				direction = SortOrder.ASC;
@@ -247,8 +246,7 @@ public class ESGeneSearch implements GeneSearch {
 
 	@Override
 	public List<Map<String, Object>> fetchByIds(String... ids) {
-		SearchRequestBuilder request = client.prepareSearch(index).setQuery(
-				new IdsQueryBuilder().addIds(ids));
+		SearchRequestBuilder request = client.prepareSearch(index).setQuery(new IdsQueryBuilder().addIds(ids));
 		SearchResponse response = request.execute().actionGet();
 		return processResults(response);
 	}
