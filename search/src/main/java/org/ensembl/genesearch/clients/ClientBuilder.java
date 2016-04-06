6d5e4ca1aae7800561022fbbadd6b97f3a798829
Dan Staines <dstaines@ebi.ac.uk>
2016-04-06 22:50:27 +0100

    create and set temporary directory as path.home

--- search/src/main/java/org/ensembl/genesearch/clients/ClientBuilder.java
+++ search/src/main/java/org/ensembl/genesearch/clients/ClientBuilder.java
@@ -3,8 +3,11 @@ package org.ensembl.genesearch.clients;
 import static org.apache.commons.lang3.StringUtils.isEmpty;
 import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
@@ -15,9 +18,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ClientBuilder {
-	
+
 	public static class ClientBuilderException extends RuntimeException {
-		
+
 		private static final long serialVersionUID = 1L;
 
 		public ClientBuilderException(String message, Throwable cause) {
@@ -31,13 +34,11 @@ public class ClientBuilder {
 		public ClientBuilderException(Throwable cause) {
 			super(cause);
 		}
-		
+
 	}
 
-	
-	private static final Logger log = LoggerFactory
-			.getLogger(ClientBuilder.class);
-	
+	private static final Logger log = LoggerFactory.getLogger(ClientBuilder.class);
+
 	private ClientBuilder() {
 	}
 
@@ -46,8 +47,7 @@ public class ClientBuilder {
 		if (params.joinCluster) {
 			client = buildClusterClient(params.clusterName, params.hostName);
 		} else if (!isEmpty(params.clusterName)) {
-			client = buildTransportClient(params.clusterName, params.hostName,
-					params.port);			
+			client = buildTransportClient(params.clusterName, params.hostName, params.port);
 		}
 		return client;
 	}
@@ -55,34 +55,28 @@ public class ClientBuilder {
 	public static Client buildClusterClient(String clusterName, String hostName) {
 		// on startup
 		log.info("Joining cluster " + clusterName);
-
-	      Settings settings = Settings.builder()
-	        .put("discovery.zen.ping.multicast.enabled", "false")
-	        .put("discovery.zen.ping.unicast.hosts", hostName)
-	        .put("path.home","/")
-	        .build();
-		Node node = nodeBuilder().data(false).client(true).clusterName(clusterName).settings(settings).build().start();
-		// close the node when we're shutdown
-		Runtime.getRuntime().addShutdownHook(new Thread(() -> 
-			node.close()
-		));
-		return node.client();
+		try {
+			Path tempDir = Files.createTempDirectory("esgenesearch_");
+			Settings settings = Settings.builder().put("discovery.zen.ping.multicast.enabled", "false")
+					.put("discovery.zen.ping.unicast.hosts", hostName).put("path.home", tempDir.toString()).build();
+			Node node = nodeBuilder().data(false).client(true).clusterName(clusterName).settings(settings).build()
+					.start();
+			// close the node when we're shutdown
+			Runtime.getRuntime().addShutdownHook(new Thread(() -> node.close()));
+			return node.client();
+		} catch (IOException e) {
+			throw new ClientBuilderException(e.getMessage(), e);
+		}
 
 	}
 
-	public static Client buildTransportClient(String clusterName, String hostName,
-			int port) {
-		Settings settings = Settings.settingsBuilder()
-				.put("cluster.name", clusterName).put("client.transport.sniff", true).build();
+	public static Client buildTransportClient(String clusterName, String hostName, int port) {
+		Settings settings = Settings.settingsBuilder().put("cluster.name", clusterName)
+				.put("client.transport.sniff", true).build();
 		log.info("Connecting to " + hostName + ":" + port);
 		try {
-			return TransportClient
-					.builder()
-					.settings(settings)
-					.build()
-					.addTransportAddress(
-							new InetSocketTransportAddress(InetAddress
-									.getByName(hostName), port));
+			return TransportClient.builder().settings(settings).build()
+					.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(hostName), port));
 		} catch (UnknownHostException e) {
 			throw new ClientBuilderException(e.getMessage(), e);
 		}
