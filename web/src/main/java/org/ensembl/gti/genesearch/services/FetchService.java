b6199daadd6d32dd16ea56024dcbb22906a484e6
Dan Staines <dstaines@ebi.ac.uk>
2016-04-07 16:53:39 +0100

    switched to using streaming output for fetch

--- web/src/main/java/org/ensembl/gti/genesearch/services/FetchService.java
+++ web/src/main/java/org/ensembl/gti/genesearch/services/FetchService.java
@@ -1,6 +1,7 @@
 package org.ensembl.gti.genesearch.services;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
 import java.util.Map;
 
@@ -9,7 +10,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,8 +21,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.fasterxml.jackson.core.JsonEncoding;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 @Path("/fetch")
@@ -34,14 +41,33 @@ public class FetchService {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Map<String, Object>> get(@BeanParam FetchParams params) {
-		return fetch(params);
+	public Response get(@BeanParam FetchParams params) {
+		return fetchAsResponse(params);
 	}
 
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Map<String, Object>> post(@RequestBody FetchParams params) throws JsonParseException, JsonMappingException, IOException {
-		return fetch(params);
+	public Response post(@RequestBody FetchParams params) throws JsonParseException, JsonMappingException, IOException {
+		return fetchAsResponse(params);
+	}
+
+	public Response fetchAsResponse(FetchParams params) {
+		List<Map<String, Object>> results = fetch(params);
+		StreamingOutput stream = new StreamingOutput() {
+			@Override
+			public void write(OutputStream os) throws IOException, WebApplicationException {
+				JsonGenerator jg = new ObjectMapper().getFactory().createGenerator(os, JsonEncoding.UTF8);
+				jg.writeStartArray();
+				for (Map<String, Object> result : results) {
+					jg.writeObject(result);
+				}
+				jg.writeEndArray();
+
+				jg.flush();
+				jg.close();
+			}
+		};
+		return Response.ok().entity(stream).type(MediaType.APPLICATION_JSON).build();
 	}
 
 	public List<Map<String, Object>> fetch(FetchParams params) {
