043905ead2844fbca7d00a165e13cd8705266c8e
Dan Staines <dstaines@ebi.ac.uk>
2016-05-03 15:22:10 +0100

    added simple/advanced toggle

--- web/src/main/resources/static/js/search.js
+++ web/src/main/resources/static/js/search.js
@@ -34,6 +34,28 @@ var setQueryExample = function(exampleName) {
 var allFields;
 var fields;
 var facetFields;
+var advancedSearch = false;
+$('.btn-toggle').click(function() {
+	
+    $(this).find('.btn').toggleClass('active');  
+    
+    if ($(this).find('.btn-primary').size()>0) {
+    	$(this).find('.btn').toggleClass('btn-primary');
+    }
+    
+    $(this).find('.btn').toggleClass('btn-default');
+    
+    advancedSearch = !advancedSearch;
+    if(advancedSearch) {
+    	$('#adv_query').show();
+    	$('#simple_query').hide();    	
+    } else {
+    	$('#adv_query').hide();
+    	$('#simple_query').show();
+    }
+});
+
+
 $(document).ready(
 		function() {
 			$('#search').hide();
@@ -101,7 +123,7 @@ $('#add_button').click(
 			var template = $('#query_template');
 			var clone = template.clone().removeClass('hide').removeAttr('id')
 					.insertBefore(template);
-			clone.find('.query_fields').select2({
+			clone.find('.query_field').select2({
 				data : fields,
 				width : "auto",
 				dropdownAutoWidth : true
@@ -112,108 +134,134 @@ $('#add_button').click(
 		});
 
 var table;
-$('#searchButton').click(function() {
-	var search = {
-		fields : [],
-		facets : [],
-		query : $('#query').val()
-	};
-	var fieldsV = $('#fields').val();
-	if (fieldsV) {
-		fieldsV.forEach(function(field) {
-			console.log(field);
-			var f = allFields[field];
-			console.trace(f);
-			search.fields.push(f);
-		});
-	}
-	var facetsV = $('#facets').val();
-	if (facetsV) {
-		facetsV.forEach(function(field) {
-			console.log(field);
-			var f = allFields[field];
-			console.trace(f);
-			search.facets.push(f);
-		});
-	}
-	var n = 0;
-	var columns = [];
-	search.fields.forEach(function(column) {
-		console.log("Creating field " + column.name);
-		columns.push({
-			data : column.displayField,
-			title : column.displayName,
-			type : 'string',
-			visible : true,
-			sortable : true,
-			targets : n++
-		});
-	});
+$('#searchButton').click(
+		function() {
 
-	var options = {
-		processing : true,
-		serverSide : true,
-		pagingType : 'simple',
-		order : [],
-		searching : false,
-		ajax : {
-			url : '/api/query',
-			type : 'POST',
-			contentType : 'application/json',
-			data : function(data) {
-				console.log("Posting data");
-				// setting sort
-				var sorts = [];
-				for (var i = 0; i < data.order.length; i++) {
-					var field = search.fields[data.order[i].column];
-					var sort = field.searchField;
-					if (data.order[i].dir == 'desc') {
-						sort = '-' + sort;
-					}
-					console.log("Sorting on " + sort);
-					sorts.push(sort);
-				}
-				console.trace(JSON.stringify(data));
-				// create post
-				return JSON.stringify({
-					query : JSON.parse(search.query),
-					fields : map(search.fields, function(f) {
-						return f.displayField
-					}),
-					facets : map(search.facets, function(f) {
-						return f.searchField
-					}),
-					sort : sorts,
-					offset : data.start,
-					limit : data.length
-				});
-			},
-			dataFilter : function(json) {
-				console.log("Filtering data");
-				response = JSON.parse(json);
-				// modify output
-				response.recordsTotal = response.resultCount;
-				response.recordsFiltered = response.resultCount;
-				response.data = response.results;
-				response.results = undefined;
-				console.log("Completed filtering data");
-				console.trace(response);
-				return JSON.stringify(response);
+			var queryStr;
+			if (advancedSearch) {
+				queryStr = $('#query').val();
+			} else {
+				var query = {};
+				// find all simple query fields and collapse together
+				$('#simple_query').find('.query_row').not('.hide').each(
+						function(i, row) {
+							var field = $(this).find('.query_field').val();
+							var val = $(this).find('.query_value').val();
+							if (query[field]) {
+								if (query[field] instanceof Array) {
+									query[field].push(val);
+								} else {
+									query[field] = [ query[field], val ];
+								}
+							} else {
+								query[field] = val;
+							}
+						});
+				queryStr = JSON.stringify(query);
 			}
-		},
-		columnDefs : columns
-	};
-	console.trace(options);
-	if (table) {
-		console.log("Destroying table");
-		table.destroy();
-		$('#results').empty();
-	}
-	console.log("Creating table");
-	table = $('#results').DataTable(options);
-	console.log("Created table");
+			var search = {
+				fields : [],
+				facets : [],
+				query : queryStr
+			};
+			var fieldsV = $('#fields').val();
+			if (fieldsV) {
+				fieldsV.forEach(function(field) {
+					console.log(field);
+					var f = allFields[field];
+					console.trace(f);
+					search.fields.push(f);
+				});
+			}
+			var facetsV = $('#facets').val();
+			if (facetsV) {
+				facetsV.forEach(function(field) {
+					console.log(field);
+					var f = allFields[field];
+					console.trace(f);
+					search.facets.push(f);
+				});
+			}
+			var n = 0;
+			var columns = [];
+			search.fields.forEach(function(column) {
+				console.log("Creating field " + column.name);
+				columns.push({
+					data : column.displayField,
+					title : column.displayName,
+					type : 'string',
+					visible : true,
+					sortable : true,
+					targets : n++
+				});
+			});
 
-});
+			console.info(JSON.stringify(search));
+
+			var options = {
+				processing : true,
+				serverSide : true,
+				pagingType : 'simple',
+				order : [],
+				searching : false,
+				ajax : {
+					url : '/api/query',
+					type : 'POST',
+					contentType : 'application/json',
+					data : function(data) {
+						console.log("Posting data");
+						// setting sort
+						var sorts = [];
+						for (var i = 0; i < data.order.length; i++) {
+							var field = search.fields[data.order[i].column];
+							var sort = field.searchField;
+							if (data.order[i].dir == 'desc') {
+								sort = '-' + sort;
+							}
+							console.log("Sorting on " + sort);
+							sorts.push(sort);
+						}
+						console.trace(JSON.stringify(data));
+						// create post
+						return JSON.stringify({
+							query : JSON.parse(search.query),
+							fields : map(search.fields, function(f) {
+								return f.displayField
+							}),
+							facets : map(search.facets, function(f) {
+								return f.searchField
+							}),
+							sort : sorts,
+							offset : data.start,
+							limit : data.length
+						});
+					},
+					dataFilter : function(json) {
+						console.log("Filtering data");
+						response = JSON.parse(json);
+						// modify output
+						response.recordsTotal = response.resultCount;
+						response.recordsFiltered = response.resultCount;
+						response.data = response.results;
+						response.results = undefined;
+						console.log("Completed filtering data");
+						console.trace(response);
+						return JSON.stringify(response);
+					}
+				},
+				columnDefs : columns
+			};
+			console.trace(options);
+			if (table) {
+				console.log("Destroying table");
+				table.destroy();
+				$('#results').empty();
+			}
+			console.log("Creating table");
+			table = $('#results').DataTable(options);
+			console.log("Created table");
+
+		});
 
 function map(objs, callback) {
 	var results = [];
