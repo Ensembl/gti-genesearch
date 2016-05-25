e9cf188d0f2122e817f9449336d45b9dee4f27f0
Dan Staines <dstaines@ebi.ac.uk>
2016-05-25 13:37:50 +0100

    first pass at flattening to a specific level

---
+++ search/src/main/java/org/ensembl/genesearch/output/ResultsRemodeller.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright [1999-2016] EMBL-European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ensembl.genesearch.output;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author dstaines
+ *
+ */
+public class ResultsRemodeller {
+	
+	/**
+	 * Flatten the supplied object to the desired level
+	 * @param input
+	 * @param targetPath e.g. a,b will flatten to b children of a children of the supplied object
+	 * @return 
+	 */
+	public static List<Map<String, Object>> flatten(Map<String, Object> input, String... targetPath) {
+		return flatten(Arrays.asList(input), StringUtils.EMPTY, targetPath);
+	}
+
+	/**
+	 * Flatten the supplied objects to the desired level. Invoked recursively.
+	 * @param input
+	 * @param baseKey current path
+	 * @param targetPath
+	 * @return
+	 */
+	protected static List<Map<String, Object>> flatten(List<Map<String, Object>> input, String baseKey, String... targetPath) {
+		if(targetPath.length==0) {
+			return input;
+		}
+		List<Map<String, Object>> output = new ArrayList<>();
+		// work out the current path e.g. b, b.c, b.c.d
+		if(StringUtils.isEmpty(baseKey)) {
+			baseKey = targetPath[0];
+		} else {
+			baseKey = baseKey + "." + targetPath[0];
+		}
+		for(Map<String,Object> obj: input) {
+			Object subobj = obj.get(baseKey);			
+			if(subobj!=null && subobj instanceof List) {
+				for(Map<String,Object> o: (List<Map<String,Object>>)subobj) {
+					// create a new object for this row from the parent
+					Map<String,Object> newObj = cloneObject(obj, Arrays.asList(baseKey));
+					for(Entry<String,Object> e: o.entrySet()) {
+						newObj.put(baseKey+"."+e.getKey(), e.getValue());
+					}
+					output.add(newObj);
+				}
+			} else {
+				output.add(obj);
+			}
+		}
+		if(targetPath.length>1) {
+			// if we still have flattening levels, recursively invoke for the remaining levels
+			return flatten(output, baseKey, ArrayUtils.subarray(targetPath, 1, targetPath.length));
+		} else {
+			// otherwise return the flattened arrays
+			return output;
+		}
+	}
+
+	/**
+	 * Recursive deep copy mechanism with exclusion list
+	 * 
+	 * @param target
+	 * @param exclude
+	 * @return
+	 */
+	protected static Map<String, Object> cloneObject(Map<String, Object> target, Collection<String> exclude) {
+		Map<String, Object> output = new HashMap<>(target.size());
+		for (Entry<String, Object> e : target.entrySet()) {
+			if (!exclude.contains(e.getKey()) && e.getValue() != null) {
+				if (e.getValue() instanceof List) {
+					List<Object> newObjs = new ArrayList<>();
+					for (Object o : (List<? extends Object>) e.getValue()) {
+						if (o instanceof Map) {
+							newObjs.add(cloneObject((Map<String, Object>) o, exclude));
+						} else {
+							newObjs.add(o);
+						}
+					}
+					output.put(e.getKey(), newObjs);
+				} else if (e.getValue() instanceof Map) {
+					output.put(e.getKey(), cloneObject((Map<String, Object>) e.getValue(), exclude));
+				} else {
+					output.put(e.getKey(), e.getValue());
+				}
+			}
+		}
+		return output;
+	}
+
+}
---
+++ search/src/test/java/org/ensembl/genesearch/output/ResultsRemodellerTest.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright [1999-2016] EMBL-European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ensembl.genesearch.output;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * @author dstaines
+ *
+ */
+public class ResultsRemodellerTest {
+
+	@Test
+	public void testCloneSimpleNoExclude() {
+		Map<String, Object> input = parseInput("{\"a\":\"1\",\"b\":[{\"c\":\"1\"},{\"c\":\"2\"}]}");
+		Map<String, Object> output = ResultsRemodeller.cloneObject(input, Collections.emptyList());
+		assertTrue(output.containsKey("a"));
+		assertTrue(output.containsKey("b"));
+		List<Map<String, Object>> b = (List<Map<String, Object>>) output.get("b");
+		assertEquals(2, b.size());
+		assertEquals("1", b.get(0).get("c"));
+		assertEquals("2", b.get(1).get("c"));
+	}
+	
+	@Test
+	public void testCloneSimpleExclude() {
+		Map<String, Object> input = parseInput("{\"a\":\"1\",\"b\":[{\"c\":\"1\"},{\"c\":\"2\"}]}");
+		Map<String, Object> output = ResultsRemodeller.cloneObject(input, Arrays.asList("b"));
+		assertTrue(output.containsKey("a"));
+		assertFalse(output.containsKey("b"));
+	}
+	
+	@Test
+	public void testCloneSimpleExcludeChild() {
+		Map<String, Object> input = parseInput("{\"a\":\"1\",\"b\":[{\"c\":\"1\"},{\"c\":\"2\"}]}");
+		Map<String, Object> output = ResultsRemodeller.cloneObject(input, Arrays.asList("c"));
+		assertTrue(output.containsKey("a"));
+		assertTrue(output.containsKey("b"));
+		List<Map<String, Object>> b = (List<Map<String, Object>>) output.get("b");
+		assertEquals(2, b.size());
+		assertFalse(b.get(0).containsKey("c"));
+		assertFalse(b.get(1).containsKey("c"));
+	}
+
+	@Test
+	public void testSimple() {
+		String input = "{\"a\":\"1\",\"b\":[{\"c\":\"1\"},{\"c\":\"2\"}]}";
+		List<Map<String, Object>> flatten = ResultsRemodeller.flatten(parseInput(input), "b");
+		assertEquals("2 flattened rows", 2, flatten.size());
+		assertEquals("1",flatten.get(0).get("a"));
+		assertEquals("1",flatten.get(0).get("b.c"));
+		assertEquals("1",flatten.get(1).get("a"));
+		assertEquals("2",flatten.get(1).get("b.c"));
+	}
+	
+	@Test
+	public void testDeep1() {
+		String input = "{\"a\":\"1\",\"b\":[{\"c\":\"1\",\"d\":[{\"e\":\"1\"},{\"e\":\"2\"}]},{\"c\":\"2\",\"d\":[{\"e\":\"3\"},{\"e\":\"4\"}]}]}";
+		List<Map<String, Object>> flatten = ResultsRemodeller.flatten(parseInput(input), "b");
+		assertEquals("2 flattened rows", 2, flatten.size());
+		assertEquals("1",flatten.get(0).get("a"));
+		assertEquals("1",flatten.get(0).get("b.c"));
+		assertEquals(2,((List)flatten.get(0).get("b.d")).size());
+		assertEquals("1",flatten.get(1).get("a"));
+		assertEquals("2",flatten.get(1).get("b.c"));
+		assertEquals(2,((List)flatten.get(1).get("b.d")).size());
+	}
+	
+	@Test
+	public void testDeep2() {
+		String input = "{\"a\":\"1\",\"b\":[{\"c\":\"1\",\"d\":[{\"e\":\"1\"},{\"e\":\"2\"}]},{\"c\":\"2\",\"d\":[{\"e\":\"3\"},{\"e\":\"4\"}]}]}";
+		List<Map<String, Object>> flatten = ResultsRemodeller.flatten(parseInput(input), "b", "d");
+		assertEquals("2 flattened rows", 4, flatten.size());
+		assertEquals("1",flatten.get(0).get("a"));
+		assertEquals("1",flatten.get(0).get("b.c"));
+		assertEquals("1",flatten.get(0).get("b.d.e"));
+		assertEquals("1",flatten.get(1).get("a"));
+		assertEquals("1",flatten.get(1).get("b.c"));
+		assertEquals("2",flatten.get(1).get("b.d.e"));
+		assertEquals("1",flatten.get(2).get("a"));
+		assertEquals("2",flatten.get(2).get("b.c"));
+		assertEquals("3",flatten.get(2).get("b.d.e"));
+		assertEquals("1",flatten.get(3).get("a"));
+		assertEquals("2",flatten.get(3).get("b.c"));
+		assertEquals("4",flatten.get(3).get("b.d.e"));
+	}
+
+	protected static Map<String, Object> parseInput(String input) {
+		try {
+			return new ObjectMapper().readValue(input, new TypeReference<Map<String, Object>>() {
+			});
+		} catch (IOException e) {
+			fail("Could not parse input");
+			return null;
+		}
+	}
+
+}
