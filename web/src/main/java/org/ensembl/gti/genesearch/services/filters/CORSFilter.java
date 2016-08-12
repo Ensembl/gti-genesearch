718b920093770cd66bda61fa837a5f596082763d
Dan Staines <dstaines@ebi.ac.uk>
2016-08-12 14:12:07 +0100

    restricted allowed methods to GET and POST

--- web/src/main/java/org/ensembl/gti/genesearch/services/filters/CORSFilter.java
+++ web/src/main/java/org/ensembl/gti/genesearch/services/filters/CORSFilter.java
@@ -16,7 +16,7 @@ public class CORSFilter implements ContainerResponseFilter {
 		response.getHeaders().add("Access-Control-Allow-Origin", "*");
 		response.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
 		response.getHeaders().add("Access-Control-Allow-Credentials", "true");
-		response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+		response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST");
 	}
 
 }
