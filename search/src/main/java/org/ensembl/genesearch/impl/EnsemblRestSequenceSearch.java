88696a39d286e049ebc24167c274277e379a2a6d
Dan Staines <dstaines@ebi.ac.uk>
2017-05-15 11:09:43 +0100

    added proxy support

--- search/src/main/java/org/ensembl/genesearch/impl/EnsemblRestSequenceSearch.java
+++ search/src/main/java/org/ensembl/genesearch/impl/EnsemblRestSequenceSearch.java
@@ -16,6 +16,8 @@
 
 package org.ensembl.genesearch.impl;
 
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -35,6 +37,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.client.RestTemplate;
 
@@ -46,174 +49,188 @@ import org.springframework.web.client.RestTemplate;
  */
 public class EnsemblRestSequenceSearch implements Search {
 
-	public final static List<String> VALID_ARGS = Arrays.asList("type", "expand_5prime", "expand_3prime", "type",
-			"format", "species");
+    private static final int CONNECT_TIMEOUT = 5000;
 
-	public final static int DEFAULT_BATCH_SIZE = 50;
+    public final static List<String> VALID_ARGS = Arrays.asList("type", "expand_5prime", "expand_3prime", "type",
+            "format", "species");
 
-	private final Logger log = LoggerFactory.getLogger(this.getClass());
+    public final static int DEFAULT_BATCH_SIZE = 50;
 
-	private final String baseUrl;
-	private final int batchSize;
-	private final RestTemplate template = new RestTemplate();
-	private final DataTypeInfo dataType;
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-	public EnsemblRestSequenceSearch(String baseUrl, DataTypeInfo dataType, int batchSize) {
-		this.baseUrl = baseUrl;
-		this.batchSize = batchSize;
-		this.dataType = dataType;
-	}
+    private final String baseUrl;
+    private final int batchSize;
+    private final RestTemplate template = new RestTemplate();
+    private final DataTypeInfo dataType;
 
-	public EnsemblRestSequenceSearch(String baseUrl, DataTypeInfo dataType) {
-		this(baseUrl, dataType, DEFAULT_BATCH_SIZE);
-	}
+    public EnsemblRestSequenceSearch(String baseUrl, DataTypeInfo dataType, int batchSize) {
+        this.baseUrl = baseUrl;
+        this.batchSize = batchSize;
+        this.dataType = dataType;
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        String proxyHost = System.getProperty("http.proxyHost");
+        String proxyPort = System.getProperty("http.proxyPort");
+        if (!StringUtils.isEmpty(proxyHost)) {
+            int port = 80;
+            if (!StringUtils.isEmpty(proxyPort)) {
+                port = Integer.valueOf(proxyPort);
+            }
+            factory.setProxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, port)));
+        }
+        factory.setConnectTimeout(CONNECT_TIMEOUT);
+        template.setRequestFactory(factory);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.ensembl.genesearch.Search#fetch(java.util.function.Consumer,
-	 * java.util.List, org.ensembl.genesearch.QueryOutput)
-	 */
-	@Override
-	public void fetch(Consumer<Map<String, Object>> consumer, List<Query> queries, QueryOutput fieldNames) {
+    public EnsemblRestSequenceSearch(String baseUrl, DataTypeInfo dataType) {
+        this(baseUrl, dataType, DEFAULT_BATCH_SIZE);
+    }
 
-		// transform the query string into a URI
-		String url = getPostUrl(queries);
-		log.info("Using base URL " + url);
-		List<String> ids = getIds(queries);
-		log.info("Searching for " + ids.size() + " ids");
-		// work through IDs in batches (REST server currently only allows 50 IDs
-		// at a time)
-		Map<String, Object> idParams = new HashMap<>();
-		int n = 0;
-		for (List<String> idList : CollectionUtils.eagerPartition(ids, batchSize)) {
-			idParams.put("ids", idList);
-			n += idList.size();
-			// pass sequences to consumer
-			log.debug("Posting " + idList.size() + " IDs to " + url);
-			try {
-				template.postForObject(url, idParams, List.class).stream().forEach(consumer);
-			} catch (HttpMessageNotReadableException e) {
-				log.warn("Could not find sequences for " + idList);
-			}
-		}
-		log.info("Completed querying " + n + " IDs");
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.ensembl.genesearch.Search#fetch(java.util.function.Consumer,
+     * java.util.List, org.ensembl.genesearch.QueryOutput)
+     */
+    @Override
+    public void fetch(Consumer<Map<String, Object>> consumer, List<Query> queries, QueryOutput fieldNames) {
 
-	}
+        // transform the query string into a URI
+        String url = getPostUrl(queries);
+        log.info("Using base URL " + url);
+        List<String> ids = getIds(queries);
+        log.info("Searching for " + ids.size() + " ids");
+        // work through IDs in batches (REST server currently only allows 50 IDs
+        // at a time)
+        Map<String, Object> idParams = new HashMap<>();
+        int n = 0;
+        for (List<String> idList : CollectionUtils.eagerPartition(ids, batchSize)) {
+            idParams.put("ids", idList);
+            n += idList.size();
+            // pass sequences to consumer
+            log.debug("Posting " + idList.size() + " IDs to " + url);
+            try {
+                template.postForObject(url, idParams, List.class).stream().forEach(consumer);
+            } catch (HttpMessageNotReadableException e) {
+                log.warn("Could not find sequences for " + idList);
+            }
+        }
+        log.info("Completed querying " + n + " IDs");
 
-	@Override
-	public List<FieldInfo> getFieldInfo(QueryOutput fields) {
-		return getDataType().getFieldInfo();
-	}
+    }
 
-	/**
-	 * Extract any query params that need to be passed to the REST service,
-	 * using VALID_ARGS as a lookup
-	 * 
-	 * @param queries
-	 * @return URL for params
-	 */
-	protected String getPostUrl(List<Query> queries) {
-		List<String> params = queries.stream()
-				.filter(q -> VALID_ARGS.contains(q.getFieldName()) && q.getValues().length == 1)
-				.map(q -> q.getFieldName() + "=" + q.getValues()[0]).collect(Collectors.toList());
-		if (!params.isEmpty()) {
-			return baseUrl + "?" + StringUtils.join(params, '&');
-		} else {
-			return baseUrl;
-		}
-	}
+    @Override
+    public List<FieldInfo> getFieldInfo(QueryOutput fields) {
+        return getDataType().getFieldInfo();
+    }
 
-	/**
-	 * extract IDs from the queries
-	 * 
-	 * @param queries
-	 * @return IDs as list
-	 */
-	protected List<String> getIds(List<Query> queries) {
-		return queries.stream().filter(q -> q.getFieldName().equals(getIdField()))
-				.flatMap(q -> Arrays.asList(q.getValues()).stream()).collect(Collectors.toList());
-	}
+    /**
+     * Extract any query params that need to be passed to the REST service,
+     * using VALID_ARGS as a lookup
+     * 
+     * @param queries
+     * @return URL for params
+     */
+    protected String getPostUrl(List<Query> queries) {
+        List<String> params = queries.stream()
+                .filter(q -> VALID_ARGS.contains(q.getFieldName()) && q.getValues().length == 1)
+                .map(q -> q.getFieldName() + "=" + q.getValues()[0]).collect(Collectors.toList());
+        if (!params.isEmpty()) {
+            return baseUrl + "?" + StringUtils.join(params, '&');
+        } else {
+            return baseUrl;
+        }
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.ensembl.genesearch.Search#fetchByIds(java.util.List,
-	 * java.lang.String[])
-	 */
-	@Override
-	public List<Map<String, Object>> fetchByIds(QueryOutput fields, String... ids) {
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * extract IDs from the queries
+     * 
+     * @param queries
+     * @return IDs as list
+     */
+    protected List<String> getIds(List<Query> queries) {
+        return queries.stream().filter(q -> q.getFieldName().equals(getIdField()))
+                .flatMap(q -> Arrays.asList(q.getValues()).stream()).collect(Collectors.toList());
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.ensembl.genesearch.Search#query(java.util.List,
-	 * org.ensembl.genesearch.QueryOutput, java.util.List, int, int,
-	 * java.util.List)
-	 */
-	@Override
-	public QueryResult query(List<Query> queries, QueryOutput output, List<String> facets, int offset, int limit,
-			List<String> sorts) {
-		throw new UnsupportedOperationException();
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.ensembl.genesearch.Search#fetchByIds(java.util.List,
+     * java.lang.String[])
+     */
+    @Override
+    public List<Map<String, Object>> fetchByIds(QueryOutput fields, String... ids) {
+        throw new UnsupportedOperationException();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.ensembl.genesearch.Search#fetchByIds(java.util.function.Consumer,
-	 * java.lang.String[])
-	 */
-	@Override
-	public void fetchByIds(Consumer<Map<String, Object>> consumer, String... ids) {
-		throw new UnsupportedOperationException();
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.ensembl.genesearch.Search#query(java.util.List,
+     * org.ensembl.genesearch.QueryOutput, java.util.List, int, int,
+     * java.util.List)
+     */
+    @Override
+    public QueryResult query(List<Query> queries, QueryOutput output, List<String> facets, int offset, int limit,
+            List<String> sorts) {
+        throw new UnsupportedOperationException();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.ensembl.genesearch.Search#select(java.lang.String, int, int)
-	 */
-	@Override
-	public QueryResult select(String name, int offset, int limit) {
-		throw new UnsupportedOperationException();
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.ensembl.genesearch.Search#fetchByIds(java.util.function.Consumer,
+     * java.lang.String[])
+     */
+    @Override
+    public void fetchByIds(Consumer<Map<String, Object>> consumer, String... ids) {
+        throw new UnsupportedOperationException();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.ensembl.genesearch.Search#getDataType()
-	 */
-	@Override
-	public DataTypeInfo getDataType() {
-		return dataType;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.ensembl.genesearch.Search#select(java.lang.String, int, int)
+     */
+    @Override
+    public QueryResult select(String name, int offset, int limit) {
+        throw new UnsupportedOperationException();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.ensembl.genesearch.Search#up()
-	 */
-	@Override
-	public boolean up() {
-		String url = baseUrl.replace("/sequence/id", "/info/ping");
-		try {
-			ResponseEntity<Map> response = template.getForEntity(url, Map.class);
-			if (!response.getStatusCode().equals(HttpStatus.OK)) {
-				log.warn(url + " returns status " + response.getStatusCode());
-			}
-			String ping = String.valueOf(response.getBody().get("ping"));
-			if (!ping.equals("1")) {
-				log.warn(url + " returns ping " + ping);
-				return false;
-			}
-			return true;
-		} catch (Exception e) {
-			log.warn("Cannot check status from " + url, e);
-			return false;
-		}
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.ensembl.genesearch.Search#getDataType()
+     */
+    @Override
+    public DataTypeInfo getDataType() {
+        return dataType;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.ensembl.genesearch.Search#up()
+     */
+    @Override
+    public boolean up() {
+        String url = baseUrl.replace("/sequence/id", "/info/ping");
+        try {
+            ResponseEntity<Map> response = template.getForEntity(url, Map.class);
+            if (!response.getStatusCode().equals(HttpStatus.OK)) {
+                log.warn(url + " returns status " + response.getStatusCode());
+            }
+            String ping = String.valueOf(response.getBody().get("ping"));
+            if (!ping.equals("1")) {
+                log.warn(url + " returns ping " + ping);
+                return false;
+            }
+            return true;
+        } catch (Exception e) {
+            log.warn("Cannot check status from " + url, e);
+            return false;
+        }
+    }
 
 }
