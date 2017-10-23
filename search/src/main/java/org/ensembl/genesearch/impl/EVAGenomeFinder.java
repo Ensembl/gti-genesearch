bffd6e9122c79cefbf0af14dfcca6965586745ee
Dan Staines <dstaines@ebi.ac.uk>
2017-10-23 14:08:11 +0100

    basic finder implementation
    code to translate from an Ensembl genome into the string used by the
    REST API

---
+++ search/src/main/java/org/ensembl/genesearch/impl/EVAGenomeFinder.java
@@ -0,0 +1,62 @@
+package org.ensembl.genesearch.impl;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.ensembl.genesearch.Query;
+import org.ensembl.genesearch.QueryOutput;
+import org.ensembl.genesearch.QueryResult;
+import org.ensembl.genesearch.Search;
+import org.ensembl.genesearch.SearchResult;
+import org.ensembl.genesearch.info.FieldType;
+import org.ensembl.genesearch.utils.DataUtils;
+
+import com.github.tomakehurst.wiremock.admin.NotFoundException;
+
+/**
+ * Utility class used by {@link EVAVariantRestSearch} to translate a standard
+ * Ensembl genome name into the code used internally by EVA
+ * 
+ * @author dstaines
+ *
+ */
+public class EVAGenomeFinder {
+
+    private final Search evaGenomeSearch;
+    private final Search ensemblGenomeSearch;
+    private final Map<String, String> genomeNames = new HashMap<>();
+
+    public EVAGenomeFinder(Search evaGenomeSearch, Search ensemblGenomeSearch) {
+        this.evaGenomeSearch = evaGenomeSearch;
+        this.ensemblGenomeSearch = ensemblGenomeSearch;
+    }
+
+    /**
+     * Look up the name used by EVA for a given Ensembl Genome
+     * 
+     * @param genomeName
+     * @return
+     */
+    public String getEVAGenomeName(String genomeName) {
+        String evaName = genomeNames.get(genomeName);
+        if (evaName == null) {
+            Map<String, Object> eGenome = ensemblGenomeSearch.fetchById(genomeName);
+            if (eGenome == null) {
+                throw new NotFoundException("Could not find details for genome " + genomeName);
+            }
+            // use the INSDC accession as the only universal linker
+            String accession = DataUtils.getObjValsForKey(eGenome, "assembly.accession").iterator().next();
+            SearchResult evaGenomeRes = evaGenomeSearch.fetch(
+                    Arrays.asList(new Query(FieldType.TERM, "assemblyAccession", accession)),
+                    new QueryOutput("taxonomyCode", "assemblyCode"));
+            if (!evaGenomeRes.getResults().isEmpty()) {
+                Map<String, Object> evaGenome = evaGenomeRes.getResults().get(0);
+                evaName = evaGenome.get("taxonomyCode") + "_" + evaGenome.get("assemblyCode");
+            }
+            genomeNames.put(genomeName, evaName);
+        }
+        return evaName;
+    }
+
+}
---
+++ search/src/test/java/org/ensembl/genesearch/impl/EVAGenomeFinderTest.java
@@ -0,0 +1,56 @@
+package org.ensembl.genesearch.impl;
+
+import java.io.IOException;
+
+import org.apache.commons.lang3.StringUtils;
+import org.ensembl.genesearch.info.DataTypeInfo;
+import org.ensembl.genesearch.test.ESTestServer;
+import org.ensembl.genesearch.utils.DataUtils;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
+
+public class EVAGenomeFinderTest {
+
+    @ClassRule
+    public static WireMockClassRule wireMockRule = new WireMockClassRule(WireMockConfiguration.options().dynamicPort());
+
+    static Logger log = LoggerFactory.getLogger(ESGenomeSearchTest.class);
+
+    static ESTestServer testServer = new ESTestServer();
+    static ESSearch ensemblGenomeSearch = new ESSearch(testServer.getClient(), ESSearch.GENES_INDEX,
+            ESSearch.GENOME_ESTYPE, DataTypeInfo.fromResource("/genomes_datatype_info.json"));
+    static EVAGenomeFinder finder;
+
+    @BeforeClass
+    public static void setUp() throws IOException {
+        // index a sample of JSON
+        log.info("Reading documents");
+        String json = DataUtils.readGzipResource("/eva_genomes.json.gz");
+        log.info("Creating test index");
+        testServer.indexTestDocs(json, ESSearch.GENOME_ESTYPE);
+        finder = new EVAGenomeFinder(new EVAGenomeRestSearch(wireMockRule.url(StringUtils.EMPTY),
+                DataTypeInfo.fromResource("/evagenomes_datatype_info.json")), ensemblGenomeSearch);
+    }
+
+    @Test
+    public void testGrch37() {
+        String name = finder.getEVAGenomeName("homo_sapiens");
+        Assert.assertEquals("Name found", "hsapiens_grch37", name);
+    }
+
+    @Test
+    public void testBacteria() {
+        // test to see what happens when we don't have a genome in EVA. Null
+        // should be returned.
+        String name = finder.getEVAGenomeName("brucella_melitensis_gca_000988815");
+        Assert.assertTrue("No name found", name == null);
+    }
+
+}
