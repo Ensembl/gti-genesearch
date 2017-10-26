2b79e36baf82a1fcfe811ae9f08ee6fdbbf48c57
Dan Staines <dstaines@ebi.ac.uk>
2017-10-26 07:13:15 +0100

    specific exception for rest interaction

---
+++ search/src/main/java/org/ensembl/genesearch/impl/RestSearchException.java
@@ -0,0 +1,35 @@
+package org.ensembl.genesearch.impl;
+
+import org.springframework.http.HttpStatus;
+
+public class RestSearchException extends RuntimeException {
+
+    private final String uri;
+    private HttpStatus statusCode;
+
+    public RestSearchException(String uri, String details, HttpStatus statusCode) {
+        super(details);
+        this.uri = uri;
+        this.setStatusCode(statusCode);
+    }
+
+    public RestSearchException(String uri, String details, Throwable e) {
+        super(details, e);
+        this.uri = uri;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public HttpStatus getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(HttpStatus statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    private static final long serialVersionUID = 1L;
+
+}
