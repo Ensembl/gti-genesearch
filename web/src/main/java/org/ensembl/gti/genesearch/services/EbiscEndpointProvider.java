a53c7e6dd184326b51e7543fc0a19c8687fd2627
Dan Staines <dstaines@ebi.ac.uk>
2018-02-20 15:53:20 +0000

    added simple htsget implementation

--- web/src/main/java/org/ensembl/gti/genesearch/services/EbiscEndpointProvider.java
+++ web/src/main/java/org/ensembl/gti/genesearch/services/EbiscEndpointProvider.java
@@ -1,6 +1,7 @@
 package org.ensembl.gti.genesearch.services;
 
 import org.ensembl.genesearch.impl.CellLineSearch;
+import org.ensembl.genesearch.impl.HtsGetSingleFileVariantSearch;
 import org.ensembl.genesearch.impl.SearchRegistry;
 import org.ensembl.genesearch.impl.SearchType;
 import org.ensembl.genesearch.info.DataTypeInfo;
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 /**
- * Implementation using Ensembl variation stored in Elasticsearch
+ * Implementation using EBiSC variation stored in EGA accessed by htsget
  * 
  * @author dstaines
  *
@@ -26,6 +27,8 @@ public class EbiscEndpointProvider extends EndpointSearchProvider {
     private String ebiscToken;
     @Value("${ebisc.ega.url}")
     private String egaUrl;
+    @Value("${ebisc.ega.accession}")
+    private String egaAccession;
 
     public EbiscEndpointProvider() {
         super();
@@ -38,8 +41,10 @@ public class EbiscEndpointProvider extends EndpointSearchProvider {
         DataTypeInfo cellLineType = DataTypeInfo.fromResource("/celllines_datatype_info.json");
         cellLineSearch = new CellLineSearch(cellLineType, ebiscUrl, ebiscUser, ebiscToken);
         reg.registerSearch(SearchType.CELL_LINES, cellLineSearch);
-        //TODO HTSget API
-        
+        // TODO HTSget API
+        DataTypeInfo variantInfo = DataTypeInfo.fromResource("/ebisc_datatype_info.json");
+        variantSearch = new HtsGetSingleFileVariantSearch(variantInfo, egaUrl, egaAccession);
+        reg.registerSearch(SearchType.VARIANTS, variantSearch);
     }
 
 }
