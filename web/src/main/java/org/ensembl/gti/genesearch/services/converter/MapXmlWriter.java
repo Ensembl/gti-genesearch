22c5b9d6801217d8d2b6b9dfb83d35c6a0ca6acd
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2019-05-28 08:55:41 +0100

    Fix MapXmlWriter tests

--- web/src/main/java/org/ensembl/gti/genesearch/services/converter/MapXmlWriter.java
+++ web/src/main/java/org/ensembl/gti/genesearch/services/converter/MapXmlWriter.java
@@ -30,107 +30,133 @@ import javax.xml.stream.XMLStreamWriter;
 
 /**
  * Utility class to write maps as XML
- * 
- * @author dstaines
  *
+ * @author dstaines
  */
 public class MapXmlWriter {
 
-	final Logger log = LoggerFactory.getLogger(MapXmlWriter.class);
+    final Logger log = LoggerFactory.getLogger(MapXmlWriter.class);
 
-	/**
-	 * Transform a map into XML
-	 * @param name
-	 * @param map
-	 * @return String in XML format
-	 * @throws XMLStreamException
-	 * @throws FactoryConfigurationError
-	 * @throws UnsupportedEncodingException
-	 */
-	public static String mapToXml(String name, Map<String,Object> map) throws XMLStreamException, FactoryConfigurationError, UnsupportedEncodingException {
-		ByteArrayOutputStream os = new ByteArrayOutputStream(); 
-		XMLStreamWriter xsw = XMLOutputFactory.newInstance().createXMLStreamWriter(os);
-		MapXmlWriter writer = new MapXmlWriter(xsw);
-		xsw.writeStartDocument();
-		writer.writeMap(name, map);
-		xsw.writeEndDocument();
-		xsw.flush();
-		xsw.close();
-		return os.toString("UTF-8");
-	}
+    /**
+     * Transform a map into XML
+     *
+     * @param name
+     * @param map
+     * @return String in XML format
+     * @throws XMLStreamException
+     * @throws FactoryConfigurationError
+     * @throws UnsupportedEncodingException
+     */
+    public static String mapToXml(String name, Map<String, Object> map) throws XMLStreamException, FactoryConfigurationError, UnsupportedEncodingException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        XMLStreamWriter xsw = XMLOutputFactory.newInstance().createXMLStreamWriter(os);
+        MapXmlWriter writer = new MapXmlWriter(xsw);
+        xsw.writeStartDocument();
+        writer.writeMap(name, map);
+        xsw.writeEndDocument();
+        xsw.flush();
+        xsw.close();
+        return os.toString("UTF-8");
+    }
 
-	private final XMLStreamWriter writer;
+    private final XMLStreamWriter writer;
+    private boolean open;
 
-	public MapXmlWriter(XMLStreamWriter writer) {
-		this.writer = writer;
-	}
+    public MapXmlWriter(XMLStreamWriter writer) {
+        this.writer = writer;
+        this.open = false;
+    }
 
-	@SuppressWarnings("unchecked")
-	public void writeObject(String name, Object data) throws XMLStreamException {
-		Class<?> clazz = data.getClass();
-		log.debug("Data received:" + data.toString());
-		if (Map.class.isAssignableFrom(clazz)) {
-			Map<String, Object> dataMap = (Map<String, Object>) data;
-			writeMap(name, dataMap);
-		} else if (Collection.class.isAssignableFrom(clazz)) {
-			Collection<Object> dataCollection = (Collection<Object>) data;
-			writeCollection(name, dataCollection);
-		} else {
-			log.debug("Default behavior ");
-			if (data != null) {
-				writer.writeAttribute(name, String.valueOf(data));
-			}
-		}
-	}
+    public void openDoc() throws XMLStreamException {
+        writer.writeStartDocument();
+    }
 
-	public void writeCollection(String name, Collection<Object> dataCollection) throws XMLStreamException {
-		if (dataCollection != null && !dataCollection.isEmpty()) {
-			log.debug("Write Collection");
-			String elemName = null;
-			if (name.endsWith("s")) {
-				elemName = name.substring(0, name.length() - 1);
-			} else {
-				elemName = name + "_elem";
-			}
-			writer.writeStartElement(name);
-			for (Object obj : dataCollection) {
-				Class<?> oClazz = obj.getClass();
-				if (Map.class.isAssignableFrom(oClazz) || List.class.isAssignableFrom(oClazz)) {
-					writeObject(elemName, obj);
-				} else {
-					writer.writeStartElement(elemName);
-					writer.writeCharacters(String.valueOf(obj));
-					writer.writeEndElement();
-				}
-			}
-			writer.writeEndElement();
-		}
-	}
 
-	public void writeMap(String name, Map<String, Object> dataMap) throws XMLStreamException {
-		if (dataMap != null && !dataMap.isEmpty()) {
-			log.debug("write Map ");
-			writer.writeStartElement(name);
-			// write attributes first
-			for (Entry<String, Object> e : dataMap.entrySet()) {
-				if (e.getValue() != null) {
-					Class<?> eClazz = e.getValue().getClass();
-					if (!Map.class.isAssignableFrom(eClazz) && !Collection.class.isAssignableFrom(eClazz)) {
-						writer.writeAttribute(e.getKey(), String.valueOf(e.getValue()));
-					}
-				}
-			}
-			// write child elements last
-			for (Entry<String, Object> e : dataMap.entrySet()) {
-				if (e.getValue() != null) {
-					Class<?> eClazz = e.getValue().getClass();
-					if (Map.class.isAssignableFrom(eClazz) || Collection.class.isAssignableFrom(eClazz)) {
-						writeObject(e.getKey(), e.getValue());
-					}
-				}
-			}
-			writer.writeEndElement();
-		}
-	}
+    public void endDoc() throws XMLStreamException {
+        writer.writeEndDocument();
+    }
+
+    public void writeData(String name, Object data) throws XMLStreamException {
+        writer.writeStartDocument();
+        writeObject(name, data);
+        writer.writeEndDocument();
+    }
+
+    @SuppressWarnings("unchecked")
+    public void writeObject(String name, Object data) throws XMLStreamException {
+        Class<?> clazz = data.getClass();
+        log.debug("Data received:" + name + "/" + data.toString());
+        if (Map.class.isAssignableFrom(clazz)) {
+            Map<String, Object> dataMap = (Map<String, Object>) data;
+            log.debug("Assignable Map " + dataMap);
+            writeMap(name, dataMap);
+        } else if (Collection.class.isAssignableFrom(clazz)) {
+            log.debug("Assignable Collection");
+            Collection<Object> dataCollection = (Collection<Object>) data;
+            writeCollection(name, dataCollection);
+        } else {
+            log.debug("Default behavior ");
+            if (data != null) {
+                writer.writeAttribute(name, String.valueOf(data));
+            } else {
+                writer.writeEndDocument();
+            }
+        }
+
+    }
+
+    public void writeCollection(String name, Collection<Object> dataCollection) throws XMLStreamException {
+        if (dataCollection != null && !dataCollection.isEmpty()) {
+            log.debug("Write Collection");
+            String elemName = null;
+            if (name.endsWith("s")) {
+                elemName = name.substring(0, name.length() - 1);
+            } else {
+                elemName = name + "_elem";
+            }
+            writer.writeStartElement(name);
+            for (Object obj : dataCollection) {
+                Class<?> oClazz = obj.getClass();
+                if (Map.class.isAssignableFrom(oClazz) || List.class.isAssignableFrom(oClazz)) {
+                    writeObject(elemName, obj);
+                } else {
+                    writer.writeStartElement(elemName);
+                    writer.writeCharacters(String.valueOf(obj));
+                    writer.writeEndElement();
+                }
+            }
+            writer.writeEndElement();
+        }
+    }
+
+    public void writeMap(String name, Map<String, Object> dataMap) throws XMLStreamException {
+        if (dataMap != null && !dataMap.isEmpty()) {
+            log.debug("write Map " + dataMap + "/" + name);
+            writer.writeStartElement(name);
+            // write attributes first
+            for (Entry<String, Object> e : dataMap.entrySet()) {
+                log.debug("Current Entry " + e);
+                if (e.getValue() != null) {
+                    Class<?> eClazz = e.getValue().getClass();
+                    log.debug("Current Value " + e.getValue());
+                    if (!Map.class.isAssignableFrom(eClazz) && !Collection.class.isAssignableFrom(eClazz)) {
+
+                        writer.writeAttribute(e.getKey(), String.valueOf(e.getValue()));
+                        log.debug("Write Attribute " + e.getValue());
+                    }
+                }
+            }
+            // write child elements last
+            for (Entry<String, Object> e : dataMap.entrySet()) {
+                if (e.getValue() != null) {
+                    Class<?> eClazz = e.getValue().getClass();
+                    if (Map.class.isAssignableFrom(eClazz) || Collection.class.isAssignableFrom(eClazz)) {
+                        writeObject(e.getKey(), e.getValue());
+                    }
+                }
+            }
+            writer.writeEndElement();
+        }
+    }
 
 }
