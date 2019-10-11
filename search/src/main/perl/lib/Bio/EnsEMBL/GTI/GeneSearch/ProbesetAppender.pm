fd87222ccca6ce51095904289e3a5ee176268af8
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2019-10-11 16:59:54 +0100

    Updated deployment configuration

---
+++ search/src/main/perl/lib/Bio/EnsEMBL/GTI/GeneSearch/ProbesetAppender.pm
@@ -0,0 +1,41 @@
+=head1 LICENSE
+
+.. See the NOTICE file distributed with this work for additional information
+   regarding copyright ownership.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+=head1 CONTACT
+
+  Please email comments or questions to the public Ensembl
+  developers list at <dev@ensembl.org>.
+
+  Questions may also be sent to the Ensembl help desk at
+  <helpdesk@ensembl.org>
+
+=head1 NAME
+
+Bio::EnsEMBL::GTI::GeneSearch::ProbesetAppender
+
+=head1 DESCRIPTION
+
+Add existing probeset to existing genomes in ES.
+
+=head1 AUTHORS
+
+Marc Chakiachvili
+
+=cut
+
+package Bio::EnsEMBL::GTI::GeneSearch::ProbesetAppender;
+use strict;
+use warnings FATAL => 'all';
+
+1;
